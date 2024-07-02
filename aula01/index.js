1fedd27ed7d4e62a0c179cd2fa41c0d138d10cbc
Mateus Alcantara <121315239+mateusdalcantara@users.noreply.github.com>
2024-07-02 06:59:16 -0300

    Add files via upload

---
+++ aula01/index.js
@@ -0,0 +1,11 @@
+console.log("Herooo 'God'");
+console.log('Herooo "God"');
+console.log(`'Herooo' "God"`);
+console.log(123456, 1551312.789101112);
+console.log(55, 18.85, "Yabadabaduuu");
+
+var nome = "teus";
+
+console.log("Meu nome é" + nome + "Estou aprendendndo");
+
+//JS usa tipagem dinâmica.
---
+++ aula03/index.js
@@ -0,0 +1,15 @@
+let nome;
+nome = "teus";
+console.log(nome);
+nome = "mat";
+console.log(nome);
+
+console.log("aaa " + nome);
+console.log("bbb", nome);
+console.log(`ccc` + nome);
+
+//não podemos criar variáveis com palavras reservadas ex: let let
+//Coloque a variável de uma maneira que seja bem evidente let nomeCliente, let idadeCliente, etc.
+//não pode conter espaço e ter traço nome-do-cliente, utilize a mesma formatação do Java CamelCase nomeDoCliente
+// JS é case-sensitive Camelo é diferente de camelo
+//não utilize var, use let para declarar variável.
